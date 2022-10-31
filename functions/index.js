84e3ea21636f9cbce158978a85c037435ae82215
supremed3v <saadsiddiqui-programmer@protonmail.com>
2022-10-31 19:22:00 +0500

    working function stripe

--- functions/index.js
+++ functions/index.js
@@ -1,31 +1,36 @@
 /* eslint-disable */
 
 const functions = require("firebase-functions");
-const stripe = require("stripe")(
+const Stripe = require("stripe");
+const stripe = Stripe(
   "sk_test_51Lyc8kAubbU9BPA8N2YZnhRd8Z0Ldqfy5Xi6IORifEja5b9gZcPnJPk6BZVC2PezQOp19fBbfPsUJmNnBLX30hRM00gsYPxvpG"
 );
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
-app.post("/payment-sheet", async (req, res) => {
-  const customer = await stripe.customers.create();
-  const ephemeralKey = await stripe.ephemeralKey.create(
-    { customer: customer.id },
-    { apiVersion: "2022-08-01" }
-  );
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: req.body,
-    currency: "usd",
-    customer: customer.id,
-  });
-  res.json({
-    paymentIntent: paymentIntent.client_secret,
-    customer: customer.id,
-    ephemeralKey: ephemeralKey.secret,
-    publishableKey:
-      "pk_test_51Lyc8kAubbU9BPA8BjH1LE6nm7PnefGDwIqxzeEVXm0IyxXhAO5Rus0MebYmZ7kTVRdJvkii4JYtwpbsVTcpSB4M00COXWYVSs",
-  });
+const PORT = 5000;
+app.use(cors({ origin: true }));
+
+app.use(express.json());
+
+app.post("/payment", async (req, res) => {
+  try {
+    let { amount } = req.body;
+    amount = parseInt(amount);
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: Math.round(amount * 100),
+      currency: "PKR",
+      payment_method_types: ["card"],
+    });
+    const clientSecret = paymentIntent.client_secret;
+    res.json({ message: "Payment initiated", clientSecret });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
+
+app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
 
 exports.expressApi = functions.https.onRequest(app);
